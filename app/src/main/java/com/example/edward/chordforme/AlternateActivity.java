4de5149855f246fd796c54064929ff08e0fa2469
eddddddy <eddy3.14159@gmail.com>
2018-02-21 11:33:22 -0500

    to lowercase

--- app/src/main/java/com/example/edward/chordforme/AlternateActivity.java
+++ app/src/main/java/com/example/edward/chordforme/AlternateActivity.java
@@ -1,9 +1,12 @@
 package com.example.edward.chordforme;
 
 import android.Manifest;
+import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import java.io.InputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 /*
 
@@ -12,6 +15,8 @@ Need different code for training and for deployment
  */
 
 public class AlternateActivity extends AppCompatActivity {
+
+    InputStream Ab5root = getResources().openRawResource(R.raw.A_flat_major_A_flat_fifth_octave);
 
     private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
     private String[] permissions = {Manifest.permission.RECORD_AUDIO};
@@ -22,9 +27,30 @@ public class AlternateActivity extends AppCompatActivity {
         setContentView(R.layout.activity_alternate);
         ActivityCompat.requestPermissions(this,permissions,REQUEST_RECORD_AUDIO_PERMISSION);
 
+        System.out.println(getBytes(Ab5root));
+
     }
 
     private void startRecording() {
+
+    }
+
+    private String getBytes(InputStream is) {
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        byte buf[] = new byte[1024];
+        int len;
+        try {
+            while ((len = is.read(buf)) != -1) {
+                outputStream.write(buf, 0, len);
+            }
+            outputStream.close();
+            is.close();
+        } catch (IOException e) {
+
+        }
+        return outputStream.toString();
 
     }
 
