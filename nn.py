3854aaa4a1db8766561eaf0c5e51523384bf76e9
James Jiang <jamesjiang52@gmail.com>
2018-02-22 00:54:01 -0500

    Added minor root, V7 root, and inversions of major

--- nn.py
+++ nn.py
@@ -1,4 +1,19 @@
 import numpy as np
 import tensorflow as tf
 
+import time
+start = time.time()
 
+#Output is list with two elements
+#First element is chord name e.g. "G#"
+#Second element is root note
+
+#testing how long it takes to read byte data
+all_lines = []
+with open('C:\\Users\\James Jiang\\Documents\\in.txt') as f:
+    for line in f:
+        bytes_str = line.rstrip('\n')[1:-1].split(", ")
+        all_lines.append([int(byte) for byte in bytes_str])
+
+print(time.time() - start)
+input()
