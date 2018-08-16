17dd852c6bfb3ab837c3eb6ba27f668219f540a1
James Jiang <jamesjiang52@gmail.com>
2018-08-16 14:44:39 -0700

    Fixed everything

--- nn/nn_chord_roots.py
+++ nn/nn_chord_roots.py
@@ -7,24 +7,43 @@ train_chord_roots = get_data.train_chord_roots
 test_data = get_data.test_data
 test_chord_roots = get_data.test_chord_roots
 
-num_hidden_1 = 1024
-num_hidden_2 = 1024
-batch_size = 1000
-learning_rate = 0.0001
-beta_1 = 0.9
-beta_2 = 0.999
-epsilon = 1e-8
+LEN_DATA = get_data.len_data
+NUM_HIDDEN_CHORD_ROOTS_1 = 1024
+NUM_HIDDEN_CHORD_ROOTS_2 = 1024
+BATCH_SIZE = 1000
+LEARNING_RATE = 0.001
+KEEP_PROB = 0.5
 
-data = tf.placeholder(tf.float32, shape=[None, get_data.len_data])
+tf.reset_default_graph()
+
+weights_chord_roots_hidden_1 = tf.Variable(tf.random_normal([LEN_DATA, NUM_HIDDEN_CHORD_ROOTS_1], stddev=1/np.sqrt(LEN_DATA)), name="weights_chord_roots_hidden_1")
+weights_chord_roots_hidden_2 = tf.Variable(tf.random_normal([NUM_HIDDEN_CHORD_ROOTS_1, NUM_HIDDEN_CHORD_ROOTS_2], stddev=1/np.sqrt(NUM_HIDDEN_CHORD_ROOTS_1)), name="weights_chord_roots_hidden_2")
+weights_chord_roots_out = tf.Variable(tf.random_normal([NUM_HIDDEN_CHORD_ROOTS_2, 12], stddev=1/np.sqrt(NUM_HIDDEN_CHORD_ROOTS_2)), name="weights_chord_roots_out")
+biases_chord_roots_hidden_1 = tf.Variable(tf.zeros(NUM_HIDDEN_CHORD_ROOTS_1), name="biases_chord_roots_hidden_1")
+biases_chord_roots_hidden_2 = tf.Variable(tf.zeros(NUM_HIDDEN_CHORD_ROOTS_2), name="biases_chord_roots_hidden_2")
+biases_chord_roots_out = tf.Variable(tf.zeros(12), name="biases_chord_roots_out")
+
+saver = tf.train.Saver(max_to_keep=1)
+sess = tf.InteractiveSession()
+
+
+try:
+    saver.restore(sess, "model/chord_roots/chord_roots_model")
+
+    weights_chord_roots_hidden_1 = tf.get_variable("weights_chord_roots_hidden_1", shape=[LEN_DATA, NUM_HIDDEN_CHORD_ROOTS_1], dtype=tf.float32, initializer=tf.random_normal_initializer(stddev=1/np.sqrt(LEN_DATA)))
+    weights_chord_roots_hidden_2 = tf.get_variable("weights_chord_roots_hidden_2", shape=[NUM_HIDDEN_CHORD_ROOTS_1, NUM_HIDDEN_CHORD_ROOTS_2], dtype=tf.float32, initializer=tf.random_normal_initializer(stddev=1/np.sqrt(NUM_HIDDEN_CHORD_ROOTS_1)))
+    weights_chord_roots_out = tf.get_variable("weights_chord_roots_out", shape=[NUM_HIDDEN_CHORD_ROOTS_1, 12], dtype=tf.float32, initializer=tf.random_normal_initializer(stddev=1/np.sqrt(NUM_HIDDEN_CHORD_ROOTS_2)))
+    biases_chord_roots_hidden_1 = tf.get_variable("biases_chord_roots_hidden_1", shape=[NUM_HIDDEN_CHORD_ROOTS_1], dtype=tf.float32, initializer=tf.zeros_initializer())
+    biases_chord_roots_hidden_2 = tf.get_variable("biases_chord_roots_hidden_2", shape=[NUM_HIDDEN_CHORD_ROOTS_2], dtype=tf.float32, initializer=tf.zeros_initializer())
+    biases_chord_roots_out = tf.get_variable("biases_chord_roots_out", shape=[12], dtype=tf.float32, initializer=tf.zeros_initializer())
+
+except:
+    sess.run(tf.global_variables_initializer())
+
+
+data = tf.placeholder(tf.float32, shape=[None, LEN_DATA])
 labels_chord_roots = tf.placeholder(tf.float32, shape=[None, 12])
 keep_prob = tf.placeholder(tf.float32)
-
-weights_chord_roots_hidden_1 = tf.Variable(tf.random_normal([get_data.len_data, num_hidden_1], stddev=1/np.sqrt(get_data.len_data)), name="weights_chord_roots_hidden_1")
-weights_chord_roots_hidden_2 = tf.Variable(tf.random_normal([num_hidden_1, num_hidden_2], stddev=1/np.sqrt(num_hidden_1)), name="weights_chord_roots_hidden_2")
-weights_chord_roots_out = tf.Variable(tf.random_normal([num_hidden_2, 12], stddev=1/np.sqrt(num_hidden_2)), name="weights_chord_roots_out")
-biases_chord_roots_hidden_1 = tf.Variable(tf.zeros(num_hidden_1), name="biases_chord_roots_hidden_1")
-biases_chord_roots_hidden_2 = tf.Variable(tf.zeros(num_hidden_2), name="biases_chord_roots_hidden_2")
-biases_chord_roots_out = tf.Variable(tf.zeros(12), name="biases_chord_roots_out")
 
 layer_chord_roots_1 = tf.nn.relu(tf.matmul(data, weights_chord_roots_hidden_1) + biases_chord_roots_hidden_1)
 layer_dropout_chord_roots_1 = tf.nn.dropout(layer_chord_roots_1, keep_prob)
@@ -34,19 +53,15 @@ out_chord_roots = tf.matmul(layer_dropout_chord_roots_2, weights_chord_roots_out
 
 predict_chord_roots_op = tf.argmax(tf.nn.softmax(out_chord_roots), axis=1)
 loss_chord_roots = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=out_chord_roots, labels=labels_chord_roots))
-train_chord_roots_op = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=beta_1, beta2=beta_2, epsilon=epsilon).minimize(loss_chord_roots)
+train_chord_roots_op = tf.train.GradientDescentOptimizer(learning_rate=LEARNING_RATE).minimize(loss_chord_roots)
 
 correct_chord_roots = tf.equal(predict_chord_roots_op, tf.argmax(test_chord_roots, 1))
 accuracy_chord_roots = tf.reduce_mean(tf.cast(correct_chord_roots, tf.float32))
 
-saver = tf.train.Saver(max_to_keep=1)
-sess = tf.InteractiveSession()
-sess.run(tf.global_variables_initializer())
-
 counter = 0
 while True:
-    batch_data, batch_labels_chord_roots = get_data.next_batch(batch_size, train_data, train_chord_roots)
-    sess.run(train_chord_roots_op, feed_dict={data: batch_data, keep_prob: 0.5, labels_chord_roots: batch_labels_chord_roots})
+    batch_data, batch_labels_chord_roots = get_data.next_batch(BATCH_SIZE, train_data, train_chord_roots)
+    sess.run(train_chord_roots_op, feed_dict={data: batch_data, keep_prob: KEEP_PROB, labels_chord_roots: batch_labels_chord_roots})
     if counter % 10000 == 0:
         saver.save(sess, "model/chord_roots/chord_roots_model", write_meta_graph=False)
         total_accuracy_chord_roots = sess.run(accuracy_chord_roots, feed_dict={data: test_data, keep_prob: 1, labels_chord_roots: test_chord_roots})
--- nn/nn_chord_types.py
+++ nn/nn_chord_types.py
@@ -8,48 +8,66 @@ train_chord_types = get_data.train_chord_types
 test_data = get_data.test_data
 test_chord_types = get_data.test_chord_types
 
-num_hidden_1 = 2048
-num_hidden_2 = 2048
-batch_size = 1000
-learning_rate = 0.001
-beta_1 = 0.9
-beta_2 = 0.999
-epsilon = 1e-8
+LEN_DATA = get_data.len_data
+NUM_HIDDEN_CHORD_TYPES_1 = 2048
+NUM_HIDDEN_CHORD_TYPES_2 = 2048
+BATCH_SIZE = 1000
+LEARNING_RATE = 0.001
+KEEP_PROB = 0.5
 
-data = tf.placeholder(tf.float32, shape=[None, get_data.len_data])
-labels_chord_types = tf.placeholder(tf.float32, shape=[None, translate.NUM_CHORD_TYPES])
+tf.reset_default_graph()
 
-weights_chord_types_hidden_1 = tf.Variable(tf.random_normal([get_data.len_data, num_hidden_1], stddev=1/np.sqrt(get_data.len_data)), name="weights_chord_types_hidden_1")
-weights_chord_types_hidden_2 = tf.Variable(tf.random_normal([num_hidden_1, num_hidden_2], stddev=1/np.sqrt(num_hidden_1)), name="weights_chord_types_hidden_2")
-weights_chord_types_out = tf.Variable(tf.random_normal([num_hidden_2, translate.NUM_CHORD_TYPES], stddev=1/np.sqrt(num_hidden_2)), name="weights_chord_types_out")
-biases_chord_types_hidden_1 = tf.Variable(tf.zeros(num_hidden_1), name="biases_chord_types_hidden_1")
-biases_chord_types_hidden_2 = tf.Variable(tf.zeros(num_hidden_2), name="biases_chord_types_hidden_2")
+weights_chord_types_hidden_1 = tf.Variable(tf.random_normal([LEN_DATA, NUM_HIDDEN_CHORD_TYPES_1], stddev=1/np.sqrt(LEN_DATA)), name="weights_chord_types_hidden_1")
+weights_chord_types_hidden_2 = tf.Variable(tf.random_normal([NUM_HIDDEN_CHORD_TYPES_1, NUM_HIDDEN_CHORD_TYPES_2], stddev=1/np.sqrt(NUM_HIDDEN_CHORD_TYPES_1)), name="weights_chord_types_hidden_2")
+weights_chord_types_out = tf.Variable(tf.random_normal([NUM_HIDDEN_CHORD_TYPES_2, translate.NUM_CHORD_TYPES], stddev=1/np.sqrt(NUM_HIDDEN_CHORD_TYPES_2)), name="weights_chord_types_out")
+biases_chord_types_hidden_1 = tf.Variable(tf.zeros(NUM_HIDDEN_CHORD_TYPES_1), name="biases_chord_types_hidden_1")
+biases_chord_types_hidden_2 = tf.Variable(tf.zeros(NUM_HIDDEN_CHORD_TYPES_2), name="biases_chord_types_hidden_2")
 biases_chord_types_out = tf.Variable(tf.zeros(translate.NUM_CHORD_TYPES), name="biases_chord_types_out")
 
+saver = tf.train.Saver(max_to_keep=1)
+sess = tf.InteractiveSession()
+
+
+try:
+    saver.restore(sess, "model/chord_types/chord_types_model")
+
+    weights_chord_types_hidden_1 = tf.get_variable("weights_chord_types_hidden_1", shape=[LEN_DATA, NUM_HIDDEN_CHORD_TYPES_1], dtype=tf.float32, initializer=tf.random_normal_initializer(stddev=1/np.sqrt(LEN_DATA)))
+    weights_chord_types_hidden_2 = tf.get_variable("weights_chord_types_hidden_2", shape=[NUM_HIDDEN_CHORD_TYPES_1, NUM_HIDDEN_CHORD_TYPES_2], dtype=tf.float32, initializer=tf.random_normal_initializer(stddev=1/np.sqrt(NUM_HIDDEN_CHORD_TYPES_1)))
+    weights_chord_types_out = tf.get_variable("weights_chord_types_out", shape=[NUM_HIDDEN_CHORD_TYPES_1, translate.NUM_CHORD_TYPES], dtype=tf.float32, initializer=tf.random_normal_initializer(stddev=1/np.sqrt(NUM_HIDDEN_CHORD_TYPES_2)))
+    biases_chord_types_hidden_1 = tf.get_variable("biases_chord_types_hidden_1", shape=[NUM_HIDDEN_CHORD_TYPES_1], dtype=tf.float32, initializer=tf.zeros_initializer())
+    biases_chord_types_hidden_2 = tf.get_variable("biases_chord_types_hidden_2", shape=[NUM_HIDDEN_CHORD_TYPES_2], dtype=tf.float32, initializer=tf.zeros_initializer())
+    biases_chord_types_out = tf.get_variable("biases_chord_types_out", shape=[translate.NUM_CHORD_TYPES], dtype=tf.float32, initializer=tf.zeros_initializer())
+
+except:
+    sess.run(tf.global_variables_initializer())
+
+
+data = tf.placeholder(tf.float32, shape=[None, LEN_DATA])
+labels_chord_types = tf.placeholder(tf.float32, shape=[None, translate.NUM_CHORD_TYPES])
+keep_prob = tf.placeholder(tf.float32)
+
 layer_chord_types_1 = tf.nn.relu(tf.matmul(data, weights_chord_types_hidden_1) + biases_chord_types_hidden_1)
-layer_chord_types_2 = tf.nn.relu(tf.matmul(layer_chord_types_1, weights_chord_types_hidden_2) + biases_chord_types_hidden_2)
-out_chord_types = tf.matmul(layer_chord_types_2, weights_chord_types_out) + biases_chord_types_out
+layer_dropout_chord_types_1 = tf.nn.dropout(layer_chord_types_1, keep_prob)
+layer_chord_types_2 = tf.nn.relu(tf.matmul(layer_dropout_chord_types_1, weights_chord_types_hidden_2) + biases_chord_types_hidden_2)
+layer_dropout_chord_types_2 = tf.nn.dropout(layer_chord_types_2, keep_prob)
+out_chord_types = tf.matmul(layer_dropout_chord_types_2, weights_chord_types_out) + biases_chord_types_out
 
 predict_chord_types_op = tf.argmax(tf.nn.softmax(out_chord_types), axis=1)
 loss_chord_types = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=out_chord_types, labels=labels_chord_types))
-train_chord_types_op = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=beta_1, beta2=beta_2, epsilon=epsilon).minimize(loss_chord_types)
+train_chord_types_op = tf.train.GradientDescentOptimizer(learning_rate=LEARNING_RATE).minimize(loss_chord_types)
 
 correct_chord_types = tf.equal(predict_chord_types_op, tf.argmax(test_chord_types, 1))
 accuracy_chord_types = tf.reduce_mean(tf.cast(correct_chord_types, tf.float32))
 
-saver = tf.train.Saver(max_to_keep=1)
-sess = tf.InteractiveSession()
-sess.run(tf.global_variables_initializer())
-
 counter = 0
 while True:
-    batch_data, batch_labels_chord_types = get_data.next_batch(batch_size, train_data, train_chord_types)
-    sess.run(train_chord_types_op, feed_dict={data: batch_data, labels_chord_types: batch_labels_chord_types})
+    batch_data, batch_labels_chord_types = get_data.next_batch(BATCH_SIZE, train_data, train_chord_types)
+    sess.run(train_chord_types_op, feed_dict={data: batch_data, keep_prob: KEEP_PROB, labels_chord_types: batch_labels_chord_types})
     if counter % 10000 == 0:
         saver.save(sess, "model/chord_types/chord_types_model", write_meta_graph=False)
-        total_accuracy_chord_types = sess.run(accuracy_chord_types, feed_dict={data: test_data, labels_chord_types: test_chord_types})
+        total_accuracy_chord_types = sess.run(accuracy_chord_types, feed_dict={data: test_data, keep_prob: 1, labels_chord_types: test_chord_types})
         print("Training step:", counter)
-        print("Evaluation accuracy:", str(round(100*total_accuracy_chord_types, 1)) + "%")
+        print("Evaluation accuracy:", str(round(100*total_accuracy_chord_types, 1)) + "%\n")
         if total_accuracy_chord_types > 0.95:
             break
     counter += 1
